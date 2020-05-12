c1439b32eba8ff9739307e4c97df4ae7efdc54fe
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-05-12 15:05:17 +0200

    Spinner added

--- app/src/main/java/com/example/sep4androidapp/fragments/mainFragment/mainViewFragments/FragmentFirstPage.java
+++ app/src/main/java/com/example/sep4androidapp/fragments/mainFragment/mainViewFragments/FragmentFirstPage.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,10 +14,23 @@ import androidx.fragment.app.Fragment;
 import com.example.sep4androidapp.R;
 
 public class FragmentFirstPage extends Fragment {
+    Spinner spinner;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_main_firstpage, container, false);
+        spinner = v.findViewById(R.id.spinner);
+
+        String[] arraySpinner = new String[] {
+                "Living room", "Bedroom", "Bedroom 2"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_spinner_item, arraySpinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
 
         return v;
     }
