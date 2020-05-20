585cdb7035c12cae00d5283a7d9f657fba5edc85
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-05-20 15:56:03 +0200

    Random data received, displayed

--- app/src/main/java/com/example/sep4androidapp/fragments/mainFragment/mainViewFragments/FragmentFirstPage.java
+++ app/src/main/java/com/example/sep4androidapp/fragments/mainFragment/mainViewFragments/FragmentFirstPage.java
@@ -1,20 +1,30 @@
 package com.example.sep4androidapp.fragments.mainFragment.mainViewFragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
+import com.example.sep4androidapp.Entities.RoomCondition;
 import com.example.sep4androidapp.R;
+import com.example.sep4androidapp.ViewModels.ReportViewModel;
 
 public class FragmentFirstPage extends Fragment {
     Spinner spinner;
+    ReportViewModel viewModel;
+    TextView currentTemperature, currentHumidity, currentCO2, currentSound, timeStamp;
+    Button updateButton;
 
     @Nullable
     @Override
@@ -30,6 +40,30 @@ public class FragmentFirstPage extends Fragment {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
+        currentTemperature = v.findViewById(R.id.currentTemperature);
+        currentHumidity = v.findViewById(R.id.currentHumidity);
+        currentSound = v.findViewById(R.id.currentSound);
+        currentCO2 = v.findViewById(R.id.currentCo2);
+        timeStamp = v.findViewById(R.id.timeStamp);
+
+        viewModel = new ViewModelProvider(this).get(ReportViewModel.class);
+        viewModel.getRoomCondition().observe(getViewLifecycleOwner(), new Observer<RoomCondition>() {
+            @Override
+            public void onChanged(RoomCondition roomCondition) {
+                  currentTemperature.setText(String.format("%.1f", roomCondition.getTemperature()) + " °C");
+                  currentCO2.setText(String.format("%.0f", roomCondition.getCo2()) + " ppm");
+                  currentHumidity.setText(String.format("%.0f", roomCondition.getHumidity()) + "%");
+                  currentSound.setText(String.format("%.0f", roomCondition.getSound()) + "dB");
+            }
+        });
+
+        updateButton = v.findViewById(R.id.updateButton);
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewModel.updateRoomCondition();
+            }
+        });
 
 
         return v;
