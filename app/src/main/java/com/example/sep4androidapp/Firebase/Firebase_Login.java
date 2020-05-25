a4296912ddbb077573c41d8d1312db800691aaae
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-05-25 15:38:15 +0200

    Small commit
    Manifest modified so that the firebase authentication runs first, not the mainActivity.

--- app/src/main/java/com/example/sep4androidapp/Firebase/Firebase_Login.java
+++ app/src/main/java/com/example/sep4androidapp/Firebase/Firebase_Login.java
@@ -42,7 +42,7 @@ public class Firebase_Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_firebase_auth);
         _mFirebaseAuth = FirebaseAuth.getInstance();
         user = _mFirebaseAuth.getCurrentUser();
 
