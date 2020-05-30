45f5f3a2fb1d902a3fb92be7eb18a146e84541d4
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-05-30 13:39:01 +0200

    Saving commit so Pouneh can merge

--- app/src/main/java/com/example/sep4androidapp/fragments/roomFragment/RoomsFragment.java
+++ app/src/main/java/com/example/sep4androidapp/fragments/roomFragment/RoomsFragment.java
@@ -5,27 +5,23 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.sep4androidapp.Adapter.RoomsAdapter;
 import com.example.sep4androidapp.Entities.Device;
+import com.example.sep4androidapp.Entities.NewDeviceModel;
 import com.example.sep4androidapp.R;
 import com.example.sep4androidapp.ViewModels.RoomsViewModel;
 import com.example.sep4androidapp.fragments.setUpDeviceFragment.SetUpDeviceFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import java.util.List;
-import java.util.Objects;
+import com.google.android.material.snackbar.Snackbar;
 
 public class RoomsFragment extends Fragment {
     private RoomsAdapter adapter;
@@ -60,8 +56,16 @@ public class RoomsFragment extends Fragment {
             @Override
             public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
                 String id = adapter.getDeviceAt(viewHolder.getAdapterPosition()).getDeviceId();
-                Log.i("TAG", "Device to delete: " + id);
+                String name = adapter.getDeviceAt(viewHolder.getAdapterPosition()).getName();
                 viewModel.deleteDevice(id);
+
+                NewDeviceModel saveDevice = new NewDeviceModel(id, name);
+
+                Snackbar.make(recyclerView, id, Snackbar.LENGTH_LONG)
+                        .setAction("Undo", v -> {
+                            viewModel.postDevice(saveDevice);
+                            adapter.notifyDataSetChanged();
+                        }).show();
             }
         }).attachToRecyclerView(recyclerView);
 
