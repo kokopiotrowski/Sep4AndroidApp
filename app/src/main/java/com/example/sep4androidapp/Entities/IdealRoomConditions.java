4885bf93b7d3c560f2c865c31d59eac938e52e69
Konrad Piotrowski <konrad.piotrowski00@gmail.com>
2020-06-05 19:04:54 +0200

    + 2 charts in report View and ideal conditions now available

---
+++ app/src/main/java/com/example/sep4androidapp/Entities/IdealRoomConditions.java
@@ -0,0 +1,24 @@
+package com.example.sep4androidapp.Entities;
+
+public class IdealRoomConditions {
+    private double co2;
+    private double humidity;
+    private double sound;
+    private double temperature;
+
+    public double getCo2() {
+        return co2;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public double getSound() {
+        return sound;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+}
--- app/src/main/java/com/example/sep4androidapp/connection/apis/ReportApi.java
+++ app/src/main/java/com/example/sep4androidapp/connection/apis/ReportApi.java
@@ -1,5 +1,6 @@
 package com.example.sep4androidapp.connection.apis;
 
+import com.example.sep4androidapp.Entities.IdealRoomConditions;
 import com.example.sep4androidapp.connection.responses.ReportResponse;
 import com.example.sep4androidapp.connection.responses.SleepDataResponse;
 
@@ -15,6 +16,9 @@ public interface ReportApi {
     @GET("reports/{deviceId}")
     Call<ReportResponse> getReport(@Path("deviceId") String deviceId, @Query("dateStart") String dateStart,
                                    @Query("dateFinish") String dateFinish);
+
+    @GET("reports/ideal/{deviceId}")
+    Call<IdealRoomConditions> getIdealConditions(@Path("deviceId") String deviceId);
 
     @GET("reports/sleeps/{sleepId}")
     Call<SleepDataResponse> getSleepData(@Path("sleepId") int sleepId);
